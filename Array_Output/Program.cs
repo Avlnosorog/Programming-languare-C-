cff47cf966e17fbf1b32b49eaaa8c533972d0e97
Alexander <nosorog-2010@yandex.ru>
2022-09-07 18:46:23 +0300

    Save Output  Array

---
+++ Array_Output/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+    Console.Write($"Количество элементов массива array {count} ");
+}
+PrintArray(array);
