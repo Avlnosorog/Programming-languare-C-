1936344153019b9c1ae61a7423659341b7c8d420
Alexander <nosorog-2010@yandex.ru>
2022-09-07 20:47:25 +0300

    Save001 Sorting Array

--- Array_Sorting_Method/Program.cs
+++ Array_Sorting_Method/Program.cs
@@ -8,7 +8,29 @@
 //     {
 //         Console.Write($"{array[i]} ");
 //     }
-//     Console.WriteLine();
-//     Console.Write($"Количество элементов массива {count} ");
 // }
+
+// //PrintArray(array);
+
+
+// void SelectionSort(int[] array)
+// {
+//     for(int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+//         for(int j=i+1; j<array.Length; j++)
+//         {
+//             if(array[j] < array[minPosition])
+//             {
+//                 minPosition = j;
+//             }
+//         }
+//         int temparray = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temparray;
+//     }
+// }
+// PrintArray(array);
+// Console.WriteLine();
+// SelectionSort(array);
 // PrintArray(array);
