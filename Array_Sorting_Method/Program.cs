be2b4601cf187a8f1cf93d7b3602b0012fbb16ae
Alexander <nosorog-2010@yandex.ru>
2022-09-07 18:40:28 +0300

    Save001 Sorting Array

--- Array_Sorting_Method/Program.cs
+++ Array_Sorting_Method/Program.cs
@@ -1,14 +1,14 @@
-﻿int[] array = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+﻿// int[] array = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
 
-void PrintArray(int[] array)
-{
-    int count = array.Length;
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
 
-    for(int i = 0; i < count; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-    Console.Write($"Количество элементов массива {count} ");
-}
-PrintArray(array);
+//     for(int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+//     Console.Write($"Количество элементов массива {count} ");
+// }
+// PrintArray(array);
